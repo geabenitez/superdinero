a727209fb5e91ebe1c334fd737bc68f48f260316
Gerson Alvarado <gerson@yes.com.sv>
2020-02-13 17:33:54 -0600

    creacion de views

--- application/views/admin/pages/questions.php
+++ application/views/admin/pages/questions.php
@@ -1,1 +1,62 @@
-QUESTIONS
+<div id="app">
+  <?php $this->load->view('admin/pages/dialogs/questionNew')  ?>
+  <el-row :gutter='15' class='mb-4' type='flex' justify='space-between'>
+    <el-col :span='6'>
+      <el-input placeholder="Buscar" size='small' v-model="searchValue" prefix-icon='el-icon-search' clearable />
+    </el-col>
+    <el-col :span='4'>
+      <el-button 
+      class='w-full'
+        type="success" 
+        icon="el-icon-plus" 
+        size='small'
+        @click='createQuestion'>
+        Nueva pregunta
+      </el-button>
+    </el-col>
+  </el-row>
+  <el-row :gutter='15'>
+    <el-col :span='24'>
+      <el-table :data="filteredQuestions" class="w-full" stripe size='mini' empty-text='No hay datos'>
+        <el-table-column prop="index" min-width="25"></el-table-column>
+        <el-table-column prop="nameES" label="Nombre ES" min-width="180"></el-table-column>
+        <el-table-column prop="nameEN" label="Nombre EN" min-width="180"></el-table-column>
+        <el-table-column min-width="260"></el-table-column>
+        <el-table-column label="Pregunta" min-width='60'>
+          <template slot-scope='scope'>
+            <span v-if="scope.row.active == '1'" class='text-green-500 font-semibold'>Activo</span>
+            <span v-else class='text-gray-500 font-semibold'>Inactivo</span>
+          </template>
+        </el-table-column>
+        <el-table-column min-width='60'>
+          <template slot-scope="scope">
+            <el-dropdown trigger="click" class='my-1'>
+              <span class="border border-gray-400 px-4 py-1 bg-white hover:bg-gray-100 rounded">
+                <i class="el-icon-more"></i>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item @click.native="editQuestion(scope.row)">
+                  <i class="el-icon-edit-outline"></i> Editar Pregunta
+                </el-dropdown-item>
+                <el-dropdown-item @click.native="changeStatus(scope.row.id, scope.row.active)">
+                  <span v-if="scope.row.active == '1'">
+                    <i class="el-icon-close"></i> Desactivar
+                  </span>
+                  <span v-else>
+                    <i class="el-icon-check"></i> Activar
+                  </span>
+                  Pregunta
+                </el-dropdown-item>
+                <el-dropdown-item divided class='font-semibold' @click.native="deleteQuestion(scope.row.id)">
+                  <span class='text-red-500 tracking-wide'>
+                    <i class="el-icon-delete"></i> Eliminar Pregunta
+                  </span>
+                </el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-col>
+  </el-row>
+</div>
