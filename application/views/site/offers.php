dd79b1fca6babe853f719455093668f9411ba056
Gerson E. Aguirre <gerson89_28@hotmail.com>
2020-02-13 19:47:20 -0600

    changes to offer.

--- application/views/site/offers.php
+++ application/views/site/offers.php
@@ -34,8 +34,8 @@
         </div>
         <div class='flex flex-col lg:flex-row items-center justify-center -mt-6 text-white tracking-wide'>
           <div class='flex flex-col items-center justify-center'>
-            <span class='text-xl uppercase'>Mejores Soluciones Financieras Del 2020</span>
-            <p class='text-sm text-center px-64'>Compara gratuitamente soluciones financieras para latinos en USA de préstamos personales, préstamos automotrices, tarjetas de crédito, préstamos para negocios, programas de manejo de deudas, hipotecas y más.</p>
+            <span class='text-base lg:text-xl uppercase text-center mb-2'>Mejores Soluciones Financieras Del 2020</span>
+            <p class='text-xs lg:text-sm text-center px-4 md:px-32 lg:px-64'>Compara gratuitamente soluciones financieras para latinos en USA de préstamos personales, préstamos automotrices, tarjetas de crédito, préstamos para negocios, programas de manejo de deudas, hipotecas y más.</p>
           </div>
         </div>
       </div>
@@ -44,8 +44,10 @@
       <span @click='spanishLang = true' class='w-1/2 md:w-auto cursor-pointer py-1 px-5 rounded-l text-white text-center' :class='{"bg-green-500": spanishLang, "text-green-500 border border-green-500": !spanishLang}'>Español</span>
       <span @click='spanishLang = false' class='w-1/2 md:w-auto cursor-pointer py-1 px-5 rounded-r text-white text-center' :class='{"bg-green-500": !spanishLang, "text-green-500 border border-green-500": spanishLang}'>English</span>
     </div>
-    <div class='flex flex-row bg-green-500 hover:bg-green-600 cursor-pointer rounded h-32 w-full my-6 items-center justify-center container mx-auto px-4 lg:px-0'>
-      <span class='font-semibold text-xl uppercase tracking-wide text-white'>CHECK MY RATE</span>
+    <div class='my-6 container mx-auto px-4 lg:px-0'>
+      <div class='flex flex-row items-center justify-center bg-green-500 hover:bg-green-600 cursor-pointer rounded h-24 lg:h-32 w-full'>
+        <span class='font-semibold text-xl uppercase tracking-wide text-white'>CHECK MY RATE</span>
+      </div>
     </div>
     <div class="flex flex-col lg:flex-row container mx-auto px-4 lg:px-0">
       <div class='lg:hidden w-full'>
@@ -139,15 +141,15 @@
     </div>
     <div class='bg-green-500 pb-4 mt-6'>
       <div class="flex flex-col container mx-auto">
-        <div class='flex flex-row items-center justify-center text-white'>
-          <img class='h-24' src="https://superdinero.org/wp-content/uploads/2017/12/SUPERDINERO-3.png" alt="">
+        <div class='flex flex-col lg:flex-row items-center justify-center text-white'>
+          <img class='h-24 -mb-6 lg:mb-0' src="https://superdinero.org/wp-content/uploads/2017/12/SUPERDINERO-3.png" alt="">
           <a href='javascript:;' class='mr-3 font-semibold text-sm tracking-wider hover:underline'>TERMINOS DE USO</a>
           <a href='javascript:;' class='mr-3 font-semibold text-sm tracking-wider hover:underline'>POLITICA DE PRIVACIDAD</a>
           <a href='javascript:;' class='mr-3 font-semibold text-sm tracking-wider hover:underline'>DIVULGACION PUBLICITARIA</a>
-          <span class='text-xs'>4530 S. Orange Blossom Trail Orlando, FL 32839</span>
+          <span class='text-xs mt-4 lg:mt-0'>4530 S. Orange Blossom Trail Orlando, FL 32839</span>
         </div>
-        <div class='flex flex-col lg:flex-row items-center justify-center -mt-6 text-white tracking-wide'>
-          <p class='px-32 text-xs text-center'><span class='font-semibold'>Loans Rate and Terms Disclosure</span>: Rates for loans provided by lenders directly on the SuperDinero platform range between 3.84-35.99% APR with terms from 24 to 84 months. Rates presented include lender discounted rates for joining in autopay and loyalty programs, where applicable. Actual rates may be different from the rates advertised and/or shown and will be based on the lender’s eligibility criteria, which include factors such as credit score, loan amount, loan term, credit usage and history, and vary based on loan purpose. The lowest rates available typically require excellent credit, and for some lenders, may be reserved for specific loan purposes and/or shorter loan terms. The origination fee charged by the lenders on our platform varies so please check the terms of your offer. Each lender has their own qualification criteria with respect to their autopay and loyalty discounts (e.g., some lenders require the borrower to elect autopay prior to loan funding in order to qualify for the autopay discount). All rates are determined by the lender and must be agreed upon between the borrower and the borrower’s chosen lender. SuperDinero is not a bank nor a lender, does not broker loans to lenders and does not make credit or loan decisions. This web page does not constitute an offer nor a solicitation to lend. For full details on any service or product you must see the terms on the financial institution’s website that is making you an offer. If you have further customer service questions, please feel free to call (888) 839-7029. An example of total amount paid on a personal loan of $20,000 for a term of 60 months at a rate of 15% would be equivalent to $28,547.92 over the 60 month life of the loan. SuperDinero may receive compensation from its partners, at no cost to you, when you click or are referred to services offered or ads located on SuperDinero’s website.</p>
+        <div class='flex flex-col lg:flex-row items-center justify-center mt-6 lg:-mt-6 text-white tracking-wide'>
+          <p class='px-4 md:px-24 lg:px-32 text-xs text-center'><span class='font-semibold'>Loans Rate and Terms Disclosure</span>: Rates for loans provided by lenders directly on the SuperDinero platform range between 3.84-35.99% APR with terms from 24 to 84 months. Rates presented include lender discounted rates for joining in autopay and loyalty programs, where applicable. Actual rates may be different from the rates advertised and/or shown and will be based on the lender’s eligibility criteria, which include factors such as credit score, loan amount, loan term, credit usage and history, and vary based on loan purpose. The lowest rates available typically require excellent credit, and for some lenders, may be reserved for specific loan purposes and/or shorter loan terms. The origination fee charged by the lenders on our platform varies so please check the terms of your offer. Each lender has their own qualification criteria with respect to their autopay and loyalty discounts (e.g., some lenders require the borrower to elect autopay prior to loan funding in order to qualify for the autopay discount). All rates are determined by the lender and must be agreed upon between the borrower and the borrower’s chosen lender. SuperDinero is not a bank nor a lender, does not broker loans to lenders and does not make credit or loan decisions. This web page does not constitute an offer nor a solicitation to lend. For full details on any service or product you must see the terms on the financial institution’s website that is making you an offer. If you have further customer service questions, please feel free to call (888) 839-7029. An example of total amount paid on a personal loan of $20,000 for a term of 60 months at a rate of 15% would be equivalent to $28,547.92 over the 60 month life of the loan. SuperDinero may receive compensation from its partners, at no cost to you, when you click or are referred to services offered or ads located on SuperDinero’s website.</p>
         </div>
       </div>
     </div>
