92af63b4890a64cd19cc82899d3813cfc57e0cb5
Gerson Alvarado <gerson@yes.com.sv>
2020-02-27 17:18:48 -0600

    partner insert

--- application/controllers/api/Partner.php
+++ application/controllers/api/Partner.php
@@ -55,46 +55,137 @@ class Partner extends REST_Controller {
   }
 
   public function index_post(){
-    $input = $this->input->post();
-    $categories = $input["categories"];
-    $states = $input["states"];
-    $amounts = $input["amounts"];
-    unset($input["categories"], $input["states"], $input["amounts"]);
+    $input = json_decode($this->input->raw_input_stream);
+
+
+    $categories = $input->categories;
+    $states = $input->states;
+    $amounts = $input->amounts;
+    $records = $input->records;
+    $credits = $input->credits;
+    $documents = $input->documents;
+
+    unset($input->categories, $input->states, $input->amounts, $input->records, $input->credits, $input->documents);
+
+    
+    foreach ($input->characteristicsES as $key => $value) {$value="'".$value."'";}
+    $input->characteristicsES = implode(",", $input->characteristicsES);
+
+    foreach ($input->characteristicsEN as $key => $value) {$value="'".$value."'";}
+    $input->characteristicsEN = implode(",", $input->characteristicsEN);
 
     $this->db->insert('partners',$input);
     $partnerId = $this->db->insert_id();
 
     $partner_categories = [];
-    foreach (explode(",", $categories) as $value) {
+    foreach ($categories as $value) {
       array_push($partner_categories, array(
         'partnerId' => $partnerId,
         'categoryId' => $value
       ));
     }
-    $this->db->insert_batch('_partners_categories', $partner_categories);
+    $this->db->insert_batch('partners_categories', $partner_categories);
+
+
+    $partner_records = [];
+    foreach ($records as $value) {
+      array_push($partner_records, array(
+        'partnerId' => $partnerId,
+        'recordId' => $value
+      ));
+    }
+    $this->db->insert_batch('partners_records', $partner_records);
+
+
+    $partner_credits = [];
+    foreach ($credits as $value) {
+      array_push($partner_credits, array(
+        'partnerId' => $partnerId,
+        'creditsId' => $value
+      ));
+    }
+    $this->db->insert_batch('partners_credits', $partner_credits);
+
+
+    $partner_documents = [];
+    foreach ($documents as $value) {
+      array_push($partner_documents, array(
+        'partnerId' => $partnerId,
+        'documentId' => $value
+      ));
+    }
+    $this->db->insert_batch('partners_documents', $partner_documents);
+
+
+
+
+
+
+
 
     $partner_states = [];
-    foreach (explode(",", $states) as $value) {
+    foreach ($states as $value) {
       array_push($partner_states, array(
         'partnerId' => $partnerId,
         'stateId' => $value
       ));
     }
-    $this->db->insert_batch('_partners_states', $partner_states);
+    $this->db->insert_batch('partners_states', $partner_states);
 
     $partner_amounts = [];
-    foreach (explode(",", $amounts) as $value) {
+    foreach ($amounts as $value) {
       array_push($partner_amounts, array(
         'partnerId' => $partnerId,
         'amountId' => $value
       ));
     }
-    $this->db->insert_batch('_partners_amounts', $partner_amounts);
+    $this->db->insert_batch('partners_amounts', $partner_amounts);
+
+
+
+
+    $partners = $this->db->get("partners")->result();
+
+    foreach ($partners as $key => $value) {
+      $c = "categories";
+      $pc = "partners_categories";
+      $s = "states";
+      $ps = "partners_states";
+      $getCategories = array(
+        $c.'.nameES',
+        $c.'.nameEN'
+      );
+      $getStates = array(
+        $s.'.nameES',
+        $s.'.nameEN',
+      );
+      $value->categories = $this->db
+      ->select($getCategories)
+      ->from($pc)
+      ->where($pc.'.partnerId', $value->id)
+      ->join($c, $c.'.id = ' . $pc . '.categoryId', 'right')
+      ->get()->result();
+      $value->states = $this->db
+      ->select($getStates)
+      ->from($ps)
+      ->where($ps.'.partnerId', $value->id)
+      ->join($s, $s.'.id = ' . $ps . '.stateId', 'right')
+      ->get()->result();
+    }
+  
+
+
 
     $response = new stdClass();
-    $response->partners = $this->db->get("partners")->result();
-    $response->msj = 'Category created successfully.';
+    $response->partners = $partners;
+    $response->msj = 'Partner created successfully.';
+    $response->success = true;
     $this->response($response, REST_Controller::HTTP_OK);
+
+
+
+
+
   } 
 
   public function index_put($id) {
