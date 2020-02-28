026d34b7930f43dc436e022a7088f324d26c28f8
Gerson Alvarado <gerson@yes.com.sv>
2020-02-27 18:01:15 -0600

    editar partner

--- application/controllers/api/Partner.php
+++ application/controllers/api/Partner.php
@@ -170,7 +170,7 @@ class Partner extends REST_Controller {
       ->join($s, $s.'.id = ' . $ps . '.stateId', 'right')
       ->get()->result();
     }
-  
+    
     $response = new stdClass();
     $response->partners = $partners;
     $response->msj = 'Partner created successfully.';
@@ -190,6 +190,162 @@ class Partner extends REST_Controller {
       $response = new stdClass();
       $response->partners = $this->db->get("partners")->result();
       foreach ($response->partners as $key => $value) {
+        $c = "categories";
+        $pc = "partners_categories";
+        $s = "states";
+        $ps = "partners_states";
+        $getCategories = array(
+          $c.'.nameES',
+          $c.'.nameEN'
+        );
+        $getStates = array(
+          $s.'.nameES',
+          $s.'.nameEN',
+        );
+        $value->categories = $this->db
+        ->select($getCategories)
+        ->from($pc)
+        ->where($pc.'.partnerId', $value->id)
+        ->join($c, $c.'.id = ' . $pc . '.categoryId', 'right')
+        ->get()->result();
+        $value->states = $this->db
+        ->select($getStates)
+        ->from($ps)
+        ->where($ps.'.partnerId', $value->id)
+        ->join($s, $s.'.id = ' . $ps . '.stateId', 'right')
+        ->get()->result();
+      }
+      $response->msj = 'Partner updated successfully.';
+      $response->success = true;
+      
+      $this->response($response, REST_Controller::HTTP_OK);
+      return;
+    }
+
+
+    $categories = $input->categories;
+    $states = $input->states;
+    $amounts = $input->amounts;
+    $records = $input->records;
+    $credits = $input->credits;
+    $documents = $input->documents;
+
+    unset($input->categories, $input->states, $input->amounts, $input->records, $input->credits, $input->documents);
+    
+    foreach ($input->characteristicsES as $key => $value) {$value="'".$value."'";}
+    $input->characteristicsES = implode(",", $input->characteristicsES);
+
+    foreach ($input->characteristicsEN as $key => $value) {$value="'".$value."'";}
+    $input->characteristicsEN = implode(",", $input->characteristicsEN);
+
+
+    $input->updated_at = date("Y-m-d h:i:s");
+    $this->db->update('partners', $input, array('id'=>$id));
+
+
+
+
+    if($this->db->get_where("partners_categories", ['partnerId' => $id])->result()){
+      $this->db->delete('partners_categories', array('partnerId'=>$id));
+    }
+
+    if($this->db->get_where("partners_states", ['partnerId' => $id])->result()){
+      $this->db->delete('partners_states', array('partnerId'=>$id));
+    }
+
+    if($this->db->get_where("partners_amounts", ['partnerId' => $id])->result()){
+      $this->db->delete('partners_amounts', array('partnerId'=>$id));
+    }
+
+    if($this->db->get_where("partners_records", ['partnerId' => $id])->result()){
+      $this->db->delete('partners_records', array('partnerId'=>$id));
+    }
+
+    if($this->db->get_where("partners_credits", ['partnerId' => $id])->result()){
+      $this->db->delete('partners_credits', array('partnerId'=>$id));
+    }
+
+    if($this->db->get_where("partners_documents", ['partnerId' => $id])->result()){
+      $this->db->delete('partners_documents', array('partnerId'=>$id));
+    }
+
+
+
+    
+    $partnerId = $id;
+
+    $partner_categories = [];
+    foreach ($categories as $value) {
+      array_push($partner_categories, array(
+        'partnerId' => $partnerId,
+        'categoryId' => $value
+      ));
+    }
+    $this->db->insert_batch('partners_categories', $partner_categories);
+
+
+    $partner_records = [];
+    foreach ($records as $value) {
+      array_push($partner_records, array(
+        'partnerId' => $partnerId,
+        'recordId' => $value
+      ));
+    }
+    $this->db->insert_batch('partners_records', $partner_records);
+
+
+    $partner_credits = [];
+    foreach ($credits as $value) {
+      array_push($partner_credits, array(
+        'partnerId' => $partnerId,
+        'creditsId' => $value
+      ));
+    }
+    $this->db->insert_batch('partners_credits', $partner_credits);
+
+
+    $partner_documents = [];
+    foreach ($documents as $value) {
+      array_push($partner_documents, array(
+        'partnerId' => $partnerId,
+        'documentId' => $value
+      ));
+    }
+    $this->db->insert_batch('partners_documents', $partner_documents);
+
+
+
+
+
+
+
+
+    $partner_states = [];
+    foreach ($states as $value) {
+      array_push($partner_states, array(
+        'partnerId' => $partnerId,
+        'stateId' => $value
+      ));
+    }
+    $this->db->insert_batch('partners_states', $partner_states);
+
+    $partner_amounts = [];
+    foreach ($amounts as $value) {
+      array_push($partner_amounts, array(
+        'partnerId' => $partnerId,
+        'amountId' => $value
+      ));
+    }
+    $this->db->insert_batch('partners_amounts', $partner_amounts);
+
+
+
+
+    
+
+    $response = new stdClass();
+    $response->partners = $this->db->get("partners")->result();
+    foreach ($response->partners as $key => $value) {
       $c = "categories";
       $pc = "partners_categories";
       $s = "states";
@@ -215,21 +371,9 @@ class Partner extends REST_Controller {
       ->join($s, $s.'.id = ' . $ps . '.stateId', 'right')
       ->get()->result();
     }
-      $response->msj = 'Partner updated successfully.';
-      $response->success = true;
-     
-      $this->response($response, REST_Controller::HTTP_OK);
-      return;
-    }
-
-    $input['updated_at'] = date("Y-m-d h:i:s");
-    $this->db->update('partners', $input, array('id'=>$id));
-
-    $response = new stdClass();
-    $response->partners = $this->db->get("partners")->result();
     $response->msj = 'Partner updated successfully.';
     $response->success = true;
-
+    
     $this->response($response, REST_Controller::HTTP_OK);
   }
 
