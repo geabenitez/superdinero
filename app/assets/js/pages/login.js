fadb0d3d6069463baa1114d0c750a9c27760d758
Gerson E. Aguirre <gersonz@yes.com.sv>
2020-04-07 17:28:36 -0600

    remove data quemada en login.

--- app/assets/js/pages/login.js
+++ app/assets/js/pages/login.js
@@ -2,25 +2,25 @@ new Vue({
   el: '#app',
   data: function () {
     return {
-      email: 'admin@superdinero.com',
-      password: '123456'
+      email: '',
+      password: ''
     }
   },
   methods: {
     processLogin(email, password) {
       axios
         .post(`${site_url}process_login`, { email, password })
-        .then(function(res){
+        .then(function (res) {
 
           if (res.data.result.success) {
             console.log('entre');
-            
-            window.location=`${site_url}admin/generator`;
-          }else{
+
+            window.location = `${site_url}admin/generator`;
+          } else {
             console.log('no entre');
           }
-          
-          
+
+
         })
     }
   }
